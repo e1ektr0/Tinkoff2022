6da766638f5dcb8a2cbfaddf40d9cf49bf64df5b
Maxim Feofilov <maxim.feofilov@gmail.com>
2022-12-03 18:45:20 +0300

    cleanup

--- ConsoleApp1/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MyNamespace
-{
-    public static class Program{
-        public static void Main()
-        {
-            new Task3.Task3Slow().Process(Console.In, Console.Out);
-        }
-    }
-}
