23842f116872d021e729b5a4ca7ec9867b42fa5b
Maxim Feofilov <maxim.feofilov@gmail.com>
2022-12-07 04:34:30 +0300

    Wrong t4 solution

--- Task4/Task4/Program.cs
+++ Task4/Task4/Program.cs
@@ -4,25 +4,67 @@ new Task4Solution().Process(Console.In, Console.Out);
 
 public class Task4Solution : IConsoleTest
 {
-    double[] x;
-    double[] y;
+    private double[] _x = null!;
+    private double[] _y = null!;
+    private int n = 0;
 
     public void Process(TextReader textReader, TextWriter textWriter)
     {
-        var n = int.Parse(textReader.ReadLine()!);
-        x = new double[n];
-        y = new double[n];
+        n = int.Parse(textReader.ReadLine()!);
+
+        PreparePoints(n);
+
+        var p0 = 0;
+        var p1 = n / 3;
+        var p2 = n * 2 / 3;
+
+
+        var s = S(p0, p1, p2);
+        s += Range(p0, p1);
+        s += Range(p1, p2);
+        s += Range(p2, p0);
+
+        textWriter.WriteLine(s);
+    }
+
+    private double Range(int p1, int p2)
+    {
+        var a = p1 - 1;
+        if (a < 0)
+            a = n - a;
+        var b = p2 - 1;
+        if (b < 0)
+            b = n - b;
+        if (a > n)
+            a -= n;
+        if (b > n)
+            b -= n;
+        
+        if (Math.Abs(a - b) < 3)
+            return 0;
+
+        var c = a + b / 2;
+        var s = S(a, b, c);
+        s += Range(a, c);
+        s += Range(b, c);
+        return s;
+    }
+
+    private void PreparePoints(int n)
+    {
+        _x = new double[n];
+        _y = new double[n];
         var alfa = 180 * (n - 2) / n;
-        double angle    = Math.PI * alfa / 180.0;
-        var cosA = Math.Cos(angle);
-        var sinA = Math.Sin(angle);
+        double angle = Math.PI * alfa / 180.0;
+        double delta = 0;
         for (int i = 1; i < n; i++)
         {
-            x[i] = x[i - 1] + cosA;
-            y[i] = y[i - 1] + sinA;
+            delta += angle;
+            var cosA = Math.Cos(delta);
+            var sinA = Math.Sin(delta);
+            _x[i] = _x[i - 1] + cosA;
+            _y[i] = _y[i - 1] + sinA;
         }
-        //todo: multiple
-        textWriter.WriteLine(S(0, 1, 2));
     }
 
     private double S(int p1, int p2, int p3)
@@ -41,6 +83,6 @@ public class Task4Solution : IConsoleTest
 
     private double L(int p1, int p2)
     {
-        return Math.Sqrt(Math.Pow(x[p2] - x[p1], 2) + Math.Pow(y[p2] - y[p1], 2));
+        return Math.Sqrt(Math.Pow(_x[p2] - _x[p1], 2) + Math.Pow(_y[p2] - _y[p1], 2));
     }
 }
--- Task4/Task4Test/Tests.cs
+++ Task4/Task4Test/Tests.cs
@@ -5,8 +5,9 @@ namespace Task4Test;
 public class Tests
 {
     [Test]
-    [TestCase(new object[] { "3" }, new object[] { "0.433013" })]
+    [TestCase(new object[] { "3" }, new object[] { "0,43301270189221897" })]
     [TestCase(new object[] { "10" }, new object[] { "3.553212" })]
+    [TestCase(new object[] { "500" }, new object[] { "3.553212" })]
     public void BaseTest(object[] inputStrings, object[] results)
     {
         var testRunner = new TestRunner<Task4Solution>();
