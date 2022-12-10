e2e2e7d684a4776f0180e1b819f7a7e02261ba06
Maxim Feofilov <maxim.feofilov@gmail.com>
2022-12-10 09:28:20 +0300

    Slow implementation

--- Task2/Task2/Task2Solution.cs
+++ Task2/Task2/Task2Solution.cs
@@ -6,18 +6,31 @@ public class Task2Solution : IConsoleTest
     {
         var readLine = textReader.ReadLine()!;
         var strings = readLine.Split();
-        var a = int.Parse(strings[0]);
-        var b = int.Parse(strings[1]);
-        var c = int.Parse(strings[2]);
+        long a = int.Parse(strings[0]);
+        long b = int.Parse(strings[1]);
+        long c = int.Parse(strings[2]);
 
         readLine = textReader.ReadLine()!;
         strings = readLine.Split();
-        var x = int.Parse(strings[0]);
-        var y = int.Parse(strings[1]);
-        var z = int.Parse(strings[2]);
+        long x = int.Parse(strings[0]);
+        long y = int.Parse(strings[1]);
+        long z = int.Parse(strings[2]);
 
-        var sum = x / a + b / y + z / c;
+        var sum = x/a + y/b + z/c;
+        var result = 0;
         
+        for (long xi = 0; xi <= sum; xi++)
+            for (long yi = 0; yi <= sum - xi; yi++)
+                result++;
+        
+        textWriter.WriteLine(result);
+    }
 
+    public int Factorial(int f)
+    {
+        if (f == 0)
+            return 1;
+        else
+            return f * Factorial(f - 1);
     }
 }
--- Task2/Task2Test/Tests.cs
+++ Task2/Task2Test/Tests.cs
@@ -12,4 +12,13 @@ public class Tests
         var testRunner = new TestRunner<Task2Solution>();
         testRunner.Process(inputStrings, results);
     }
+    
+    [Test]
+    public void BigTest()
+    {
+        
+        var testRunner = new TestRunner<Task2Solution>();
+        var balances = $"{1e9} {1e9} {1e9}" ;
+        testRunner.Process(new object[] { "1 1 1", balances}, new object[] { "10" });
+    }
 }
