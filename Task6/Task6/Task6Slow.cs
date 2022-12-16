7134045088a675e0c196e28a2ee4b825fb355e78
Maxim Feofilov <maxim.feofilov@gmail.com>
2022-12-16 19:32:33 +0300

    split files

---
+++ Task6/Task6/Task6Slow.cs
@@ -0,0 +1,31 @@
+﻿using Common;
+
+namespace Task6;
+
+public class Task6Slow : IConsoleTest
+{
+    List<int> _list = new();
+    public void Process(TextReader textReader, TextWriter textWriter)
+    {   var tree = new Tree();
+        var q = long.Parse(textReader.ReadLine()!);
+        for (int i = 0; i < q; i++)
+        {
+            int k = int.Parse(textReader.ReadLine()!);
+            _list.Add(k);
+            var max = 0;
+            foreach (var l in _list)
+            {
+                foreach (var l1 in _list)
+                {
+                    var max1 = l ^ l1;
+                    if (max1 > max)
+                    {
+                        max = max1;
+                    }
+                }
+            }
+            textWriter.WriteLine(tree.Add(k));
+        }
+        
+    }
+}
--- Task6/Task6/Task6Solution.cs
+++ Task6/Task6/Task6Solution.cs
@@ -2,33 +2,6 @@
 
 namespace Task6;
 
-public class Task6Slow : IConsoleTest
-{
-    List<int> _list = new();
-    public void Process(TextReader textReader, TextWriter textWriter)
-    {   var tree = new Tree();
-        var q = long.Parse(textReader.ReadLine()!);
-        for (int i = 0; i < q; i++)
-        {
-            int k = int.Parse(textReader.ReadLine()!);
-            _list.Add(k);
-            var max = 0;
-            foreach (var l in _list)
-            {
-                foreach (var l1 in _list)
-                {
-                    var max1 = l ^ l1;
-                    if (max1 > max)
-                    {
-                        max = max1;
-                    }
-                }
-            }
-            textWriter.WriteLine(tree.Add(k));
-        }
-        
-    }
-}
 public class Node
 {
     public Node? Right { get; set; }
