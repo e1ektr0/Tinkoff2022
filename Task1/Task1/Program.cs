4451a451bf61827c48229368f2fc6b062480b19b
Maxim Feofilov <maxim.feofilov@gmail.com>
2022-12-17 22:59:13 +0300

    To c# 7 task 1

--- Task1/Task1/Program.cs
+++ Task1/Task1/Program.cs
@@ -1,1 +1,49 @@
-﻿new Task1.Task1().Process(Console.In, Console.Out);
+﻿using Common;
+
+namespace Task1
+{
+    public static class Program{
+        public static void Main()
+        {
+            new Task1().Process(Console.In, Console.Out);
+        }
+    }
+    
+    public class Task1 : IConsoleTest
+    {
+        public void Process(TextReader textReader, TextWriter textWriter)
+        {
+            var n = int.Parse(textReader.ReadLine());
+            var s = textReader.ReadLine();
+            var b = textReader.ReadLine();
+
+
+            var result = 0;
+            var lastSymbol = '#';
+            var skipBeforeSpace = false;
+            for (int i = 0; i < n; i++)
+            {
+                if (s[i] == ' ')
+                {
+                    lastSymbol = '#';
+                    skipBeforeSpace = false;
+                }
+                else
+                {
+                    if (skipBeforeSpace)
+                        continue;
+
+                    if (b[i] == lastSymbol)
+                    {
+                        skipBeforeSpace = true;
+                        result++;
+                    }
+
+                    lastSymbol = b[i];
+                }
+            }
+
+            textWriter.WriteLine(result);
+        }
+    }
+}
